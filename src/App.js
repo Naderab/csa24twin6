ba8544448b7d12145bfe96d9b3b5623ad4aa3f14
Naderab <abdellaouinader@gmail.com>
2023-01-27 17:34:00 +0100

    séance 1 intro+ecmascript6

--- src/App.js
+++ src/App.js
@@ -2,12 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function hoisting(){
+    console.log(a)
+    var a=10;
+    console.log(a)
+  }
+  //hoisting();
+  const num =90;
+  console.log(num)
+  //num = 77;
+  let x =10;
+  x=100;
+  if(x>5){
+    var y =5;
+    const z=20;
+    console.log(y+z)
+    console.log(x+y)
+  }
+  //console.log(z)
+  console.log(y)
+
+  let f = nom=> console.log(nom)
+  f("test")
+
+  const t = [1,2,3,4]
+  let newt=t.filter(p=>p<2)
+  console.log(newt)
+
+  const users = [
+    {name:'mohamed',age:15},
+    {name:'salma',age:20},
+    {name:'kamel',age:16},
+    {name:'mariem',age:50}
+  ]
+  const [{name,age},p2,p3,p4]=users
+  const testAdulte = users.map(person => person.age > 18 ? console.log(`${person.name} est un adulte`):console.log(`${person.name} n'est pas un adulte`))
+  //console.log(p1)
+//const {name,age} = p1
+console.log(name,age)
+const classe = {
+  niveau:"4twin",
+  nbEtudiant:15
+}
+const newClasse = {...classe,email:"4twin@esprit.tn"}
+console.log(newClasse)
+const newUsers = [...users,{name:'spreadTest',age:50}]
+console.log(newUsers)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>4twin6</code> and save to reload.
         </p>
         <a
           className="App-link"
